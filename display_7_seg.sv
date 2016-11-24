c0ae304d239543e94fd64f05782a13bd9cd347b7
Cicera Rodrigues <cicera_mrodrigues@hotmail.com>
2016-11-24 16:44:29 -0300

    Display 7 segmentos de 0 a F

--- display_7_seg.sv
+++ display_7_seg.sv
@@ -13,6 +13,15 @@ case [bcd]
 4'b0111:ser_seg=7'b1110000;
 4'b1000:ser_seg=7'b1111111;
 4'b1001:ser_seg=7'b1111011;
+4'b1010:ser_seg=7'b1110111;
+4'b1011:ser_seg=7'b0011111;
+4'b1100:ser_seg=7'b1001110;
+4'b1101:ser_seg=7'b0111101;
+4'b1110:ser_seg=7'b1001111;
+4'b1111:ser_seg=7'b0111110;
+default = ser_seg=7'b1111110;
+
+
 
 endcase
 endmodule
