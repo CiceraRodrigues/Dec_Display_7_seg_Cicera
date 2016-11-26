372379d851f85c15696ef18b0e9591f51b50433b
Cicera Rodrigues <cicera_mrodrigues@hotmail.com>
2016-11-26 00:06:32 -0300

    Teste para o display

---
+++ Teste.sv
@@ -0,0 +1,24 @@
+module Teste;
+    logic clk, rst;
+    wire[3:0] bcd;
+	 wire [6:0] ser_seg
+
+    initial begin
+        clk = 0;
+        rst = 1;
+
+        #10;
+        rst = 0;
+       
+
+
+        repeat(100) begin
+            if(clk)
+
+            bcd=bcd+1
+            clk = ~clk;
+            #10;
+        end
+    end
+
+endmodule
